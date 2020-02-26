e18760dfaeeef531efb70e2b73ce92cd5f6dba8c
Martin Kosír <martin@martinkosir.net>
2020-02-26 17:17:12 +0100

    Update the example cv data

--- resources.js
+++ resources.js
@@ -1,154 +1,300 @@
 window.EMPTY_CV_DATA = {
-  "publishUrl": "",
-  "publishStatus": "",
+  "id": "",
+  "general": {
+    "city": "",
+    "icon": "ion-person",
+    "name": "Personal info",
+    "postal": "",
+    "street": "",
+    "country": "",
+    "birthKey": "Date of birth",
+    "emailKey": "Email address",
+    "phoneKey": "Phone number",
+    "backTitle": "",
+    "birthDate": "1594-12-09",
+    "addressKey": "Address",
+    "emailValue": "hello@kickresume.com",
+    "frontTitle": "",
+    "phoneValue": "(999)-999-9999",
+    "websiteKey": "Web",
+    "avatarShown": true,
+    "description": "<p>I am a sales professional oriented at customer satisfaction, with extensive experience in Cyber Security. I am targeted at improving customer experience, and at the same time have thorough understanding of the industry's technical frameworks. I believe my skills and experience could be benefitial for your company.</p>",
+    "lastNameKey": "Last name",
+    "nationality": "Swedish",
+    "firstNameKey": "First name",
+    "websiteValue": "www.kickresume.com",
+    "lastNameValue": "Adolf",
+    "descriptionKey": "Profile",
+    "firstNameValue": "Gustaf II ",
+    "nationalityKey": "Nationality"
+  },
+  "isCover": false,
+  "isResume": true,
+  "sections": [
+    {
+      "data": {
+        "type": "work",
+        "entries": [
+          {
+            "city": "Stockholm",
+            "uuid": "f8d49347-9336-4357-b0b3-6d85c6bd3c85",
+            "company": "Cyber Security Services",
+            "country": "Sweden",
+            "current": true,
+            "endDate": [
+              "",
+              ""
+            ],
+            "jobTitle": "Sales Account Manager",
+            "startDate": [
+              "",
+              "2014"
+            ],
+            "description": "<p>Working with an established Cyber Security Managed Services company, contributing to their sales expansion project by:</p><ul><li>setting up a sales strategy for a new range of services</li><li>acquiring new clients</li><li>developing long - term business relationships</li><li>meeting and exceeding the sales target</li><li>generating a sustainable pipeline of business</li></ul>",
+            "presentValue": "present"
+          },
+          {
+            "city": "Hamburg",
+            "uuid": "337e2a52-5f67-4f3e-99d7-798406205825",
+            "company": "F5",
+            "country": "Germany",
+            "current": false,
+            "endDate": [
+              "",
+              "2014"
+            ],
+            "jobTitle": "Sales Engineer",
+            "startDate": [
+              "",
+              "2009"
+            ],
+            "description": "<p>Developed my knowledge of marketing strategies and customer service in the field of Cyber Security. My task included:</p><ul><li>providing solution designs and system engineering configurations</li><li>designing technical solutions to solve customer business problems and create new service offering</li><li>maintaining knowledge of of competitive landscape</li></ul><p><br></p><p>Customer support:</p><ul><li>providing highly professional customer support during pre-sales and post-sales activities</li><li>delivering sales pitch/whiteboards</li><li>partnering with sales team to develop and manage Technical Account Plan and Territory Account Plans.</li><li>Influencing sales strategy</li></ul>",
+            "presentValue": "present"
+          },
+          {
+            "city": "Malmo",
+            "uuid": "b915adc5-c691-41a5-899c-68fe3063468d",
+            "company": "ABC Corporation",
+            "country": "Sweden",
+            "current": false,
+            "endDate": [
+              "",
+              "2009"
+            ],
+            "jobTitle": "Security Specialist",
+            "startDate": [
+              "",
+              "2005"
+            ],
+            "description": "<ul><li>Analyzed and established security requirements for networks.</li><li>Identified abnormalities and reported violations.</li><li>Performed vulnerability testing, risk analyses and security assessments.</li><li>Trained and supervised fellow employees in security awareness, protocols and procedures.</li><li>Researched and recommended security upgrades.</li><li>Designed and conducted security audits to ensure operational security.</li></ul>",
+            "presentValue": "present"
+          }
+        ]
+      },
+      "icon": "ion-briefcase",
+      "name": "Work experience",
+      "type": "experience",
+      "uuid": "35feba00-ab15-47aa-aea9-d5eac1a10c26"
+    },
+    {
+      "data": {
+        "type": "education",
+        "entries": [
+          {
+            "city": "Stockholm",
+            "uuid": "f8f30f0e-32e8-48b8-b0eb-20e5229922f9",
+            "company": "Stockholm University",
+            "country": "Sweden",
+            "current": false,
+            "endDate": [
+              "",
+              "2005"
+            ],
+            "jobTitle": "Information Security",
+            "startDate": [
+              "",
+              "2003"
+            ],
+            "description": "<p>Completed a Master's degree with courses on:</p><ul><li>Information Security in Organizations</li><li>Network Security</li><li>Information Security Project Management</li><li>Cyber Security</li><li>Cyber Forensics</li></ul>",
+            "presentValue": "present"
+          },
+          {
+            "city": "Uppsala",
+            "uuid": "25d12183-22ce-49bd-b14c-f1cef2f9c791",
+            "company": "Uppsala Universitet",
+            "country": "Sweden",
+            "current": false,
+            "endDate": [
+              "",
+              "2003"
+            ],
+            "jobTitle": "Computer Science",
+            "startDate": [
+              "",
+              "2000"
+            ],
+            "description": "<p>Completed a Bachelor's degree with courses in:</p><ul><li>Artificial intelligence</li><li>Human-computer interaction</li><li>Programming languages</li><li>Data mining</li><li>Data Communication</li><li>Computer Architecture</li><li>Software engineering</li></ul>",
+            "presentValue": "present"
+          }
+        ]
+      },
+      "icon": "ion-university",
+      "name": "Education",
+      "type": "experience",
+      "uuid": "5fa8c347-8eca-4d7c-857d-f608711fcb07"
+    },
+    {
+      "data": {
+        "entries": [
+          "Analytical",
+          "Attention to Detail",
+          "Mathematical Skills",
+          "Problem-solving",
+          "Independent worker",
+          "Customer-focused",
+          "Outgoing",
+          "Negotiation",
+          "Sales"
+        ]
+      },
+      "icon": "ion-star",
+      "name": "Strengths",
+      "type": "keyword",
+      "uuid": "a8f4e52b-d524-4e79-9df0-72dad0665264"
+    },
+    {
+      "data": [
+        {
+          "name": "Programing Languages",
+          "uuid": "a0977bd6-62f9-442b-bc54-3b7ab190c9cc",
+          "skills": [
+            {
+              "name": "C++",
+              "shown": "level",
+              "skillLevel": "5",
+              "description": ""
+            },
+            {
+              "name": "Python",
+              "shown": "level",
+              "skillLevel": "4",
+              "description": ""
+            },
+            {
+              "name": "Java",
+              "shown": "level",
+              "skillLevel": "3",
+              "description": ""
+            }
+          ]
+        },
+        {
+          "name": "Platforms",
+          "uuid": "1d5fd2c2-6b70-452f-8dc2-b45707277023",
+          "skills": [
+            {
+              "name": "Windows ",
+              "shown": "level",
+              "skillLevel": "5",
+              "description": ""
+            },
+            {
+              "name": "Mac OS",
+              "shown": "level",
+              "skillLevel": "5",
+              "description": ""
+            },
+            {
+              "name": "UNIX",
+              "shown": "level",
+              "skillLevel": "3",
+              "description": ""
+            },
+            {
+              "name": "Linux",
+              "shown": "level",
+              "skillLevel": "2",
+              "description": ""
+            }
+          ]
+        },
+        {
+          "name": "Tools",
+          "uuid": "51562dfa-b702-42c8-80d3-c8d6de45adee",
+          "skills": [
+            {
+              "name": "Microsoft Office",
+              "shown": "level",
+              "skillLevel": "5",
+              "description": ""
+            },
+            {
+              "name": "ISS RealSecure",
+              "shown": "level",
+              "skillLevel": "5",
+              "description": ""
+            },
+            {
+              "name": "Checkpoint Firewall",
+              "shown": "level",
+              "skillLevel": "4",
+              "description": ""
+            },
+            {
+              "name": "Norton Virus Protection Utilities",
+              "shown": "level",
+              "skillLevel": "3",
+              "description": ""
+            }
+          ]
+        }
+      ],
+      "icon": "ion-erlenmeyer-flask",
+      "name": "Skills",
+      "type": "skill",
+      "uuid": "34c77b5a-dc33-477f-b477-44e747e98485"
+    }
+  ],
   "settings": {
-    "webBackground": "bg01",
-    "color": "gre",
+    "name": "Cyber Security Account Manager Resume Sample",
+    "color": "lim",
+    "format": 20,
+    "fontFace": "pla-pts",
+    "fontSize": "fs-1-4",
+    "template": "midnight",
     "dateFormat": "dd/mm/yyyy",
+    "downloadCv": 0,
+    "lineHeight": "lh-1-1",
     "pageFormat": "a4",
     "addressFormat": {
       "order": [
         [
-          "name",
+          "name"
         ],
         [
-          "address",
+          "address"
         ],
         [
           "city",
-          "postal",
+          "postal"
         ],
         [
-          "country",
-        ],
+          "country"
+        ]
       ],
-      "delimiter": " ",
+      "delimiter": " "
     },
-    "name": "My CV",
-    "fontFace": "rob-rob",
-    "fontSize": "fs-1-4",
-    "format": 20,
-    "template": "basic",
-    "lineHeight": "lh-1-1",
-    "downloadCv": 0,
+    "webBackground": "bg01"
   },
-  "thumbnailUrl": "",
+  "avatarUrl": "https://s3-eu-west-1.amazonaws.com/avatars-kickresume-com/resume-avatars/275890/d075141e98542d55b38bfc77761e0c05.jpg",
+  "isWebsite": false,
+  "publishUrl": "",
+  "isProofread": "",
   "isPublished": "",
-  "isBeingProofread": false,
-  "general": {
-    "birthKey": "Date of birth",
-    "street": "",
-    "phoneValue": "",
-    "descriptionKey": "Profile",
-    "emailKey": "Email address",
-    "nationalityKey": "Nationality",
-    "firstNameKey": "First name",
-    "websiteKey": "Web",
-    "postal": "",
-    "city": "",
-    "addressKey": "Address",
-    "name": "Personal info",
-    "birthDate": "",
-    "frontTitle": "",
-    "lastNameValue": "",
-    "backTitle": "",
-    "phoneKey": "Phone number",
-    "emailValue": "",
-    "firstNameValue": "",
-    "websiteValue": "",
-    "country": "",
-    "avatarShown": false,
-    "icon": "ion-person",
-    "nationality": "",
-    "description": "",
-    "lastNameKey": "Last name",
-  },
-  "publishTemplate": "paper",
-  "isCover": false,
   "creationDate": "",
   "lastEditDate": "",
-  "sections": [
-    {
-      "name": "Work experience",
-      "data": {
-        "entries": [
-          {
-            "endDate": [
-              "",
-              "",
-            ],
-            "city": "",
-            "startDate": [
-              "",
-              "",
-            ],
-            "presentValue": "present",
-            "country": "",
-            "current": false,
-            "jobTitle": "",
-            "uuid": "bfe5fd45-c58d-4797-8bd7-b87107ba51a2",
-            "description": "",
-            "company": "",
-          },
-        ],
-        "type": "work",
-      },
-      "type": "experience",
-      "icon": "ion-briefcase",
-      "uuid": "796cb788-9b44-42f9-b8c2-726816d3dc62",
-    },
-    {
-      "name": "Education",
-      "data": {
-        "entries": [
-          {
-            "endDate": [
-              "",
-              "",
-            ],
-            "city": "",
-            "startDate": [
-              "",
-              "",
-            ],
-            "presentValue": "present",
-            "country": "",
-            "current": false,
-            "jobTitle": "",
-            "uuid": "65e97593-0e77-45c2-a40f-00602adce144",
-            "description": "",
-            "company": "",
-          },
-        ],
-        "type": "education",
-      },
-      "type": "experience",
-      "icon": "ion-university",
-      "uuid": "7278c661-92c8-4a97-9f1f-b26732ae5228",
-    },
-    {
-      "name": "Skills",
-      "data": [
-        {
-          "skills": [
-            {
-              "name": "",
-              "description": "",
-              "shown": "level",
-              "skillLevel": 0,
-            },
-          ],
-          "name": "",
-        },
-      ],
-      "type": "skill",
-      "icon": "ion-erlenmeyer-flask",
-      "uuid": "efc196e7-003c-4683-83eb-afd54fe1804a",
-    },
-  ],
-  "isProofread": "",
-  "isWebsite": false,
-  "avatarUrl": "",
-  "isResume": true,
-  "id": "",
+  "thumbnailUrl": "",
+  "publishStatus": "",
+  "publishTemplate": "paper",
+  "isBeingProofread": false
 };
